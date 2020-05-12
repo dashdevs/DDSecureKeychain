22c629a2c68ffe1c6abada9ed6ca182f34044d36
Yakov Shkolnikov <yakov.shkolnikov@itomy.ch>
2020-05-12 10:02:55 +0300

    DDOS-230 - refactoring

--- SecureKeychain/Classes/KeychainItem/KeychainItem.swift
+++ SecureKeychain/Classes/KeychainItem/KeychainItem.swift
@@ -11,6 +11,8 @@ public protocol KeychainItem {
     func clear()
     subscript(key: String) -> String? { get set }
     
+    @discardableResult
     mutating func append(_ accessGroup: String) -> Self
+    @discardableResult
     mutating func append(_ accessibility: KeychainItemAccessibility) -> Self
 }
